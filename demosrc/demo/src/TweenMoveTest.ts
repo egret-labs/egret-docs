b69d7237b33067c6f9b595ca2d10a8fa99179f7f
mebiusashan <mebiusashan@gmail.com>
2014-07-04 11:38:15 +0800

    添加了一个事件的demo和两个动画的demo

---
+++ demosrc/demo/src/TweenMoveTest.ts
@@ -0,0 +1,28 @@
+class TweenMoveTest extends egret.DisplayObjectContainer
+{
+
+    public constructor()
+    {
+        super();
+        this.addEventListener(egret.Event.ADDED_TO_STAGE,this.onAddToStage,this);
+    }
+
+    private onAddToStage(event:egret.Event)
+    {
+
+        //绘制一个透明度为1的绿色矩形，宽高为100*100
+        var spr1:egret.Sprite = new egret.Sprite();
+        spr1.graphics.beginFill(0x00ff00, 1);
+        spr1.graphics.drawRect(0, 0, 100, 100);
+        spr1.graphics.endFill();
+        spr1.width = 100;
+        spr1.height = 80;
+        spr1.x = 50;
+        spr1.y = 50;
+        this.addChild( spr1 );
+
+        egret.Tween.get(spr1, { loop: true }).to({ x: 250 }, 2000).to({ x: 50 }, 2000);
+
+    }
+
+}
